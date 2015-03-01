e382b1d8b23b9283a54551a6633b41d26f1fdfd7
Quarzi <kim.jan.andersen82@gmail.com>
2015-03-01 17:22:06 +0100

    Initial Commit

---
+++ Food4FunEngine/F4fIngredient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Food4FunEngine
+{
+    public class F4fIngredient
+    {
+        private int _id;
+        private int _name;
+        private int _carbs;
+        private int _fat;
+        private int _protein;
+
+
+
+        public int Carbs
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Fat
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Protein
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Food4FunEngine/F4fMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Food4FunEngine
+{
+    public class F4fMenu
+    {
+        public F4fRecipe F4fRecipes
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
