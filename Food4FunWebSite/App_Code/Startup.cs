ecf1734a2de3f02f46d468c6d56505c7695fdf85
Quizzarex <kent.stark.olsen@gmail.com>
2015-03-12 19:51:19 +0100

    WebSite added

---
+++ Food4FunWebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Food4FunWebSite.Startup))]
+namespace Food4FunWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
